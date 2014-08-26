559df53fcd2ecf98949a2a508459a83a201429e5
b87775 <jwoods1@fullsail.edu>
2014-08-26 05:47:06 -0600

    added angular site neeed style and content

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var app = angular.module("app", ["ngRoute","ngAnimate"]);
+
+app.config(function($routeProvider) {
+    $routeProvider.when('/',
+        {
+            templateUrl: 'partials/main.html',
+            controller: "AppCtrl"
+        }
+    )
+        .when('/register',
+        {
+            templateUrl: 'partials/register.html'
+
+        }
+    )
+        .when('/history',
+        {
+            templateUrl: 'partials/history.html',
+            controller: "AppCtrl"
+        }
+    )
+        .when('/gallery',
+        {
+            templateUrl: 'partials/gallery.html',
+            controller: "AppCtrl"
+        }
+    )
+        .when('/members',
+        {
+            templateUrl: 'partials/members.html',
+            controller: "AppCtrl"
+        }
+    )
+});
+
+app.controller("AppCtrl", function($scope) {
+    $scope.model = {
+        message: "This is my app!!!"
+    }
+});
